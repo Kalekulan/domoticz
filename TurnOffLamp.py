b63606a705a8b43259c2885b91c812713c6b48bc
Kalekulan <lukas.foughman@gmail.com>
2017-01-20 21:57:50 +0100

    Adding logging

--- TurnOffLamp.py
+++ TurnOffLamp.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import argparse
+import logging as log
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--process", dest="processPath",
@@ -11,16 +12,24 @@ args = parser.parse_args()
 #if args.process:
 	#print("process = " + args.process)
 if args.verbose:
-    print("verbose turned on")
+    log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
+    log.info("Verbose output.")
+else:
+    log.basicConfig(format="%(levelname)s: %(message)s")
 
 switchId='13' #Dimmer in right window
 switchCmd='Off'
 
 #http://domoticz.local:8080/json.htm?type=command&param=switchlight&idx=13&switchcmd=Off&level=0&passcode=
 url = 'http://domoticz.local:8080/json.htm?type=command&param=switchlight&idx=' + switchId + '&switchcmd=' + switchCmd + '&level=0&passcode='
-r = requests.get(url)
+log.info("url = " + url)
+#r = requests.get(url)
+r = requests.get(url, stream=True)
+#log.info("request response = " + r)
 r.json()
+#log.info("json = " + r.json())
 #os.startfile('E:/Origin/Games/Battlefield 1/bf1.exe')
 os.startfile(args.processPath)
+#os.system("pause")
 #os.open('E:/Origin/Games/Battlefield 1/bf1.exe')
 #os.wait()
